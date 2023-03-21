8c79be218a98d546f0df6ef3b8d8c508cfff9fa3
DevGustavoCoelho07 <84469800+DevGustavoCoelho07@users.noreply.github.com>
2023-03-21 08:31:13 -0300

    Methods to check if stack is empty, pop stack items

--- stack.js
+++ stack.js
@@ -5,12 +5,23 @@ class Stack {
   add(pilhas) {
     this.pilha.push(pilhas);
   }
+  pop() {
+    const popped = this.pilha.pop(this.pilha);
+    console.log(`Removido ${popped}`);
+  }
   peek() {
     console.log(`Topo da Pilha: ${this.pilha[this.pilha.length - 1]}`);
+  }
+
+  isEmpty() {
+    if (this.pilha) return console.log(!this.pilha.length);
   }
 }
 stack = new Stack();
 stack.add("C");
 stack.add("C++");
+stack.add("C#");
+stack.pop();
 stack.peek();
+stack.isEmpty();
 console.log(stack);
